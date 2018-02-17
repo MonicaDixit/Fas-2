040128decc42b503b37ee1f7cbc7149d64f53621
dixmu01 <mudita.dixit@ca.com>
2018-02-16 19:25:06 -0800

    code cleanup

--- app/solution/solution.module.ts
+++ app/solution/solution.module.ts
@@ -1,11 +1,10 @@
-import { NgModule } from "@angular/core";
-import { SharedModule } from "../shared/shared.module";
-import { SolutionComponent } from "./solution.component";
-import { CostComponent } from "./cost.component";
+import { NgModule } from '@angular/core';
+import { SharedModule } from '../shared/shared.module';
+import { SolutionComponent } from './solution.component';
 
 @NgModule({
    imports: [SharedModule],
-   declarations: [SolutionComponent, CostComponent],
-   exports: [SolutionComponent, CostComponent]
+   declarations: [SolutionComponent],
+   exports: [SolutionComponent]
 })
 export class SolutionModule {}
